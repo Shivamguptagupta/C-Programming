bf0926c5fffee6fa688dcf502ab2eff997e4d776
Shivamguptagupta <118105459+Shivamguptagupta@users.noreply.github.com>
2023-01-13 21:48:16 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,22 +1,17 @@
-#include<stdio.h>
-
-int main(){
-    int i,j,n,rows,columns;
-    int a[20][20];
-    printf("Enter the value of rows and columns\n");
-    scanf("%d%d",&rows,&columns);
-    for(i=0;i<rows;i++){
-        for(j=0;j<columns;j++){
-            printf("Enter element [%d][%d]",i,j);
-            scanf("%d",&a[i][j]);
-        }
-    }
-    printf("Matrix in tabular form :\n");
-        for(i=0;i<rows;i++){
-        for(j=0;j<columns;j++){
-            printf("%d\t",a[i][j]);
-        }
-        printf("\n");
-    }
-     return 0;
-}
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+ char a[100];
+ int length;
+ char str[100];
+ int i;
+ printf("Enter the String\n");
+ gets(str);
+ for (i = 0; str[i] != '\0'; ++i);
+ printf("Length of String = %d\n", i);
+ printf("enter a string again to calculate its length using strlen function\n");
+ scanf("%s", a);
+ length = strlen(a);
+ printf("Length of the string = %d\n", length);
+ return 0;}
